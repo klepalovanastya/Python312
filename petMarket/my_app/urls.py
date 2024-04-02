d2d6d9f52368f812d906ed5f81beffc0e48b1c52
klepalovanastya <117816858+klepalovanastya@users.noreply.github.com>
2024-04-02 17:23:42 +0300

    Add files via upload

--- petMarket/my_app/urls.py
+++ petMarket/my_app/urls.py
@@ -5,6 +5,7 @@ import my_app.views as my_app
 urlpatterns = [
     path('', my_app.mainpage),
     path('artwork/<slug:picture_name_slug>/', my_app.picture),
-    path('product/<int:product_id>/', my_app.products)
+    path('product/<int:product_id>/', my_app.products),
+    path('news/', my_app.page_news),
 
 ]
